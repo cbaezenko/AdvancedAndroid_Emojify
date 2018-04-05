bc70416dbf2fdb65bd536e58e66eb0e27f9fa199
Singorenko <cbaezenko@gmail.com>
2018-04-05 11:52:09 +0300

    detecting eyes and smiles

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -1,18 +1,18 @@
 /*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.emojify;
 
@@ -54,15 +54,28 @@ class Emojifier {
         Log.d(LOG_TAG, "detectFaces: number of faces = " + faces.size());
 
         // If there are no faces detected, show a Toast message
-        if(faces.size() == 0){
+        if (faces.size() == 0) {
             Toast.makeText(context, R.string.no_faces_message, Toast.LENGTH_SHORT).show();
         }
 
         // TODO (2): Iterate through the faces, calling getClassifications() for each face.
-
+        else {
+            for (int i = 0; i < faces.size(); i++) {
+                getClassifications(faces.valueAt(i));
+            }
+        }
         // Release the detector
         detector.release();
     }
 
     // TODO (1): Create a static method called getClassifications() which logs the probability of each eye being open and that the person is smiling.
+    private static void getClassifications(Face detector) {
+        float prob_left_open = detector.getIsLeftEyeOpenProbability();
+        float prob_right_open = detector.getIsRightEyeOpenProbability();
+        float prob_smiling = detector.getIsSmilingProbability();
+
+        Log.d(LOG_TAG, "left eye open probability: " + prob_left_open
+                + "\nright eye open probability: " + prob_right_open
+                + "\nsmiling probability: " + prob_smiling);
+    }
 }
